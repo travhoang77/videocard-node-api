db4732fc7f4a31f840273981c01e341e577d5353
travhoanng77 <travhoang77>
2021-09-29 00:19:17 -0700

    added config/index.js for heroku without strings

--- config/index.js
+++ config/index.js
@@ -1,23 +1,8 @@
-// let config = {
-//     dbUrl:
-//       process.env.DBURL ||
-//       "mongodb+srv://MRKIMBERLYS:xJEUhe2xk4aAAl22@cluster0.ntqbo.mongodb.net/videocard-dev?retryWrites=true&w=majority",
-//     port: process.env.PORT || 5000,
-//     secret: process.env.SECRET || "mysecret",
-//     env: process.env.NODE_ENV || "development",
-//     logDir: process.env.LOGDIR || "logs",
-//     viewEngine: process.env.VIEW_ENGINE || "html",
-//   };
-
-//   module.exports = config;
-
 let config = {
-  dbUrl:
-    process.env.DBURL ||
-    "mongodb+srv://MRKIMBERLYS:xJEUhe2xk4aAAl22@cluster0.ntqbo.mongodb.net/videocard-dev?retryWrites=true&w=majority",
-  port: process.env.PORT || 3000,
-  secret: process.env.SECRET || "mysecret",
-  env: process.env.NODE_ENV || "production",
+  dbUrl: process.env.DBURL,
+  port: process.env.PORT,
+  secret: process.env.SECRET,
+  env: process.env.NODE_ENV,
   logDir: process.env.LOGDIR || "logs",
   viewEngine: process.env.VIEW_ENGINE || "html",
 };
